fede9df4826c4ae31fb435a160a5afededbd0ec0
Dan Souza <souza.music@outlook.com>
2022-02-25 12:06:47 -0800

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded',  () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    list(e.target.new_task_description.value)
+  })
+})
+
+function list(newTask){
+    const p = document.createElement('p')
+    const butt = document.createElement('button')
+    p.textContent = `${newTask} `
+    butt.textContent = 'x'
+    butt.addEventListener('click', handleDelete)
+    p.appendChild(butt)
+    
+    document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
+
